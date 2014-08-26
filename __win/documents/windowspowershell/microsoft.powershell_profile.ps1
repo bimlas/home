83cfe39b89b2f00f2f45bbead638ec401a26d633
BimbaLaszlo <bimbalaszlo@gmail.com>
2014-08-26 15:57:18 +0200

    powershell - magyarazat hozzaadasa

--- __win/documents/windowspowershell/microsoft.powershell_profile.ps1
+++ __win/documents/windowspowershell/microsoft.powershell_profile.ps1
@@ -1,7 +1,7 @@
 # conemu /cmd "powershell -ExecutionPolicy Bypass"
 # ========== BimbaLaszlo(.github.io|gmail.com) =========== 2014.08.25 18:05 ==
 
-#                              GET-MYMODULE                               {{{1
+#                        IMPORT / INSTALL MODULES                         {{{1
 # ============================================================================
 
 Function Get-MyModule
@@ -11,7 +11,7 @@ Function Get-MyModule
     {
       if( Get-Module -ListAvailable | Where-Object { $_.name -eq $name } )
       {
-        Import-Module -Name $name
+        Import-Module -name $name
         $true
       }
       else
@@ -25,18 +25,14 @@ Function Get-MyModule
     }
 }
 
-#                                  MAIN                                   {{{1
-# ============================================================================
-
+# "Package-manager" for powershell.
 if( -not (Get-Mymodule -name "PsGet") )
 {
   (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 }
 
-if( -not (Get-Mymodule -name "posh-git") )
+# Git commandline-completion and prompt string.
+if( -not (Get-Mymodule -name "Posh-Git") )
 {
   Install-Module Posh-Git
 }
-
-# Load posh-git example profile
-. "$env:userprofile\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1"
