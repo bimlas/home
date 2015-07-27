834273c274ebe6c9d53593831c1254a117310a7b
BimbaLaszlo <bimbalaszlo@gmail.com>
2015-07-27 18:10:22 +0200

    update

--- linux/home/bin/makemyvim.sh
+++ linux/home/bin/makemyvim.sh
@@ -1,24 +1,22 @@
-# #!/bin/sh - build vim - execute in source root
+#!/bin/sh
+# build vim - execute in source root
 #
 # https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source
 #
-# Try out withou gnome.
-# sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev        \
-#     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev                         \
-#     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev \
-#     ruby-dev
+# apt-get build-dep vim
+# apt-get install ruby-dev python-dev python3-dev perl-dev lua-dev
 
-./configure --with-features=huge                                                    \
-            --enable-multibyte                                                      \
-            --with-compiledby="BimbaLaszlo"                                         \
-            --enable-rubyinterp                                                     \
-            --enable-pythoninterp                                                   \
-            --with-python-config-dir=/usr/lib/python2.7/config-i386-linux-gnu       \
-            --enable-python3interp                                                  \
-            --with-python3-config-dir=/usr/lib/python3.4/config-3.4m-i386-linux-gnu \
-            --enable-perlinterp                                                     \
-            --enable-luainterp                                                      \
-            --with-lua-prefix=/usr/lib/i386-linux-gnu                               \
+./configure --with-features=huge                                                      \
+            --enable-multibyte                                                        \
+            --with-compiledby="BimbaLaszlo"                                           \
+            --enable-rubyinterp                                                       \
+            --enable-pythoninterp                                                     \
+            --enable-python3interp                                                    \
+            --enable-perlinterp                                                       \
+            --enable-luainterp                                                        \
             --enable-gui=gtk2 --enable-cscope --prefix=/usr
 
-echo 'RUN MAKE'
+            # --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu       \
+            # --with-python3-config-dir=/usr/lib/python3.4/config-3.4m-x86_64-linux-gnu \
+            # --with-lua-prefix=/usr/lib/x86_64-linux-gnu                               \
+echo 'RUN `make`, `make install`'
