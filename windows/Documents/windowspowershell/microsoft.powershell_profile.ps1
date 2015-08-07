76d7c477751ab683a2d867219723b48f6c219288
BimbaLaszlo <bimbalaszlo@gmail.com>
2015-08-07 08:31:25 +0200

    powershell: Improve
    windows/system_setup.reg
    
      Have to enable Windows Update.

--- windows/Documents/windowspowershell/microsoft.powershell_profile.ps1
+++ windows/Documents/windowspowershell/microsoft.powershell_profile.ps1
@@ -1,7 +1,31 @@
-# conemu /cmd "powershell -ExecutionPolicy Bypass"
-# ========== BimbaLaszlo(.github.io|gmail.com) =========== 2014.08.25 18:05 ==
+# Powershell Improved
+#
+# First You need to install Powershell 3:
+#
+#   Powershell 3
+#   http://www.microsoft.com/en-us/download/details.aspx?id=34595
+#     x86 Windows6.1-KB2506143-x86.msu
+#     x64 Windows6.1-KB2506143-x64.msu
+#
+# Powershell 3 needs Service Pack 1 and (at least) .Net 4.0:
+#
+#   Service Pack 1
+#   http://windows.microsoft.com/en-us/windows7/install-windows-7-service-pack-1
+#
+#   .Net 4.0
+#   http://www.microsoft.com/en-us/download/details.aspx?id=17851
+#
+# To get the plugins work set ExecutionPolicy to Bypass via the command line:
+#
+#   powershell -ExecutionPolicy Bypass
+#
+# ... or set it permanent by running Powershell as Administrator, then:
+#
+#   Set-ExecutionPolicy -ExecutionPolicy Bypass
+#
+# ========== BimbaLaszlo (.github.io|gmail.com) ========== 2015.08.07 08:29 ==
 
-#                        IMPORT / INSTALL MODULES                         {{{1
+#                    "PACKAGE-MANAGER" FOR POWERSHELL                     {{{1
 # ============================================================================
 
 Function Get-MyModule
@@ -25,14 +49,38 @@ Function Get-MyModule
     }
 }
 
-# "Package-manager" for powershell.
 if( -not (Get-Mymodule -name "PsGet") )
 {
   (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 }
 
+#                               PSREADLINE                                {{{1
+# ============================================================================
+
+if( -not (Get-Mymodule -name "PSReadLine") )
+{
+  Install-Module PSReadLine
+}
+
+# Bash-like editing.
+Set-PSReadlineOption -EditMode Emacs
+
+#                                POSH-GIT                                 {{{1
+# ============================================================================
+#
 # Git commandline-completion and prompt string.
+
 if( -not (Get-Mymodule -name "Posh-Git") )
 {
   Install-Module Posh-Git
 }
+
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+    Write-Host($pwd.ProviderPath) -nonewline
+    Write-VcsStatus
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return "`n> "
+}
+
+Pop-Location
