f54da441d78ca3ed8110217f08b6eefee7d3217a
BimbaLaszlo <bimbalaszlo@gmail.com>
2015-10-29 15:57:05 +0100

    bin: Clean up

--- linux/home/bin/kepeket_negyzetesse.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir kesz
-for i in *.jpg *.png *.bmp *.gif
-  do avconv -v 20 -i $i -vf "pad=width=max(iw\,ih)+1:height=max(iw\,ih)+1:x=(ow-iw)/2:y=(oh-ih)/2:color=white" kesz/$i
-done
