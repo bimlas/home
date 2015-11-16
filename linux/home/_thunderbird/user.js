26b1471fedc4de3de48392fc2f685365aaca678a
BimbaLaszlo <bimbalaszlo@gmail.com>
2015-11-16 15:18:08 +0100

    thunderbird: Include orig mail when answering

--- linux/home/_thunderbird/user.js
+++ linux/home/_thunderbird/user.js
@@ -12,8 +12,8 @@
 user_pref("mailnews.reply_in_default_charset", true);
 user_pref("mailnews.send_default_charset", "UTF-8");
 user_pref("mailnews.view_default_charset", "UTF-8");
-// Ne idezze az eredeti uzenetet valaszolaskor.
-user_pref("mail.identity.id1.auto_quote", false);
+// Idezze az eredeti uzenetet valaszolaskor.
+user_pref("mail.identity.id1.auto_quote", true);
 // Alairas.
 user_pref("mail.identity.id1.htmlSigFormat", true);
 user_pref("mail.identity.id1.htmlSigText", "http://BimbaLaszlo.github.io/");
