dc92e5bf729f98c437f95b22f01e7eb45ff7e5c5
BimbaLaszlo <bimbalaszlo@gmail.com>
2015-12-14 15:56:39 +0100

    far: Update

---
+++ windows/AppData/Roaming/Far Manager/Profile/Macros/internal/ShellAutoCompletion_Tab.lua
@@ -0,0 +1,6 @@
+Macro {
+  description="Next";
+  area="ShellAutoCompletion"; key="Tab";
+  flags="NotEmptyCommandLine";
+  code="Keys(\"Down\")";
+}
---
+++ windows/AppData/Roaming/Far Manager/Profile/Macros/internal/Viewer_Q.lua
@@ -0,0 +1,6 @@
+Macro {
+  description="Close viewer";
+  area="Viewer"; key="Q";
+  flags="";
+  code="Keys(\"Esc\")";
+}
