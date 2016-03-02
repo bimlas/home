ca58b14fd10958ac91c92e9822e7f7b09689993b
BimbaLaszlo <bimbalaszlo@gmail.com>
2016-03-02 21:43:09 +0100

    posh: PSReadLine: Use Vi-mode

--- windows/Documents/windowspowershell/microsoft.powershell_profile.ps1
+++ windows/Documents/windowspowershell/microsoft.powershell_profile.ps1
@@ -70,16 +70,27 @@ if( -not (Get-Mymodule -name "PsGet") )
 #                               PSREADLINE                                {{{2
 # ____________________________________________________________________________
 #
-# Bash-like behaviour.
+# Linux-like behaviour.
 
 if( -not (Get-Mymodule -name "PSReadLine") )
 {
   Install-Module PSReadLine
 }
 
-# Bash-like editing.
-Set-PSReadlineOption -EditMode Emacs
-Set-PSReadlineKeyHandler -Key Ctrl+J         -Function AcceptLine
+# Vim-like editing.
+Set-PSReadlineOption -EditMode Vi
+# Buggy with my prompt.
+# Set-PSReadlineOption -ViModeIndicator Prompt
+Set-PSReadlineKeyHandler -Key Ctrl+J -Function AcceptLine
+Set-PSReadlineKeyHandler -Key Ctrl+O -Function ViCommandMode
+Set-PSReadlineKeyHandler -Key Tab    -Function Complete
+
+# Zsh-like history search.
+Set-PSReadlineKeyHandler -Key UpArrow   -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key Ctrl+P    -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key Ctrl+N    -Function HistorySearchForward
+
 Set-PSReadlineKeyHandler -Key 'Shift+PageUp','Shift+PageDown' `
                          -BriefDescription ScrollDisplayUpDownHalfPage `
                          -LongDescription "Scroll the display up or down by half page" `
@@ -95,12 +106,6 @@ Set-PSReadlineKeyHandler -Key 'Shift+PageUp','Shift+PageDown' `
   $newTop = [Math]::Min([Console]::BufferHeight - [Console]::WindowHeight, $newTop)
   [Console]::SetWindowPosition(0, $newTop)
 }
-
-# Zsh-like history search.
-Set-PSReadlineKeyHandler -Key UpArrow   -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key Ctrl+P    -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadlineKeyHandler -Key Ctrl+N    -Function HistorySearchForward
 
 #                                POSH-GIT                                 {{{2
 # ____________________________________________________________________________
