372c89aef29c81fe3887ab735de464e22e2989b3
bimlas <bimbalaszlo@gmail.com>
2016-11-21 11:25:11 +0100

    !bin_localhostincwd: Pass port as argument

--- linux/home/bin/localhostincwd.rb
+++ linux/home/bin/localhostincwd.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/ruby
 # localhostincwd.rb: Run webserver in current directory
+#
 # https://github.com/mrdoob/three.js/wiki/How-to-run-things-locally
 #
 # The dir is available on http://localhost:8000
+# The port can be passed as argument:
+#
+#   $ localhostincwd.rb 1234
+#   # http://localhost:1234
 
 require 'webrick'
 
-server = WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => Dir.pwd)
+server = WEBrick::HTTPServer.new(:Port => ARGV.empty? ? 8000 : ARGV.first, :DocumentRoot => Dir.pwd)
 
 # Add subdir, don't forget to add trailing `/` to URL:
 # http://localhost:8000/subdir/
