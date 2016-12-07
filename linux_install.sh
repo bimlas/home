4503204c2b2cce8d25e7d21f8c57ce960172288a
bimlas <bimbalaszlo@gmail.com>
2016-12-07 08:24:03 +0100

    Use aptitude instead of apt-get
    http://askubuntu.com/a/1749

--- linux_install.sh
+++ linux_install.sh
@@ -2,29 +2,31 @@
 # linux_install.sh: Install everything
 
 sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt-get install -y aptitude
+sudo aptitude safe-upgrade -y
 
 # __ BASIC STUFF ________________________
 
-sudo apt-get install -y aptitude
-sudo apt-get install -y zsh
-sudo apt-get install -y git
-sudo apt-get install -y vim
+sudo aptitude install -y aptitude
+sudo aptitude install -y zsh
+sudo aptitude install -y git
+sudo aptitude install -y vim
 
-echo 'Change shell to Zsh needs password'
+echo '!! Change shell to Zsh needs password'
 chsh -s /bin/zsh
 
 # __ DEVELOPEMENT _______________________
 
-sudo apt-get install -y gcc make
-sudo apt-get install -y ctags
+sudo aptitude install -y gcc make
+sudo aptitude install -y ctags
 
-sudo apt-get install -y python python-pip python3 python3-pip
+sudo aptitude install -y python python-pip python3 python3-pip
 
-sudo apt-get install -y ruby ruby-dev
+sudo aptitude install -y ruby ruby-dev
 sudo gem install ripper-tags gem-ripper-tags
 sudo gem install pry
 
 # __ DESKTOP ____________________________
 
-sudo apt-get install -y xorg awesome
+sudo aptitude install -y xorg
+# sudo aptitude install -y awesome
