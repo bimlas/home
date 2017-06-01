c08c079a7911a9844ae8dd60f4ab5bbb4c2ca32f
bimlas <bimbalaszlo@gmail.com>
2017-06-01 22:58:49 +0200

    !git_shdo: Adding tags works

--- linux/home/bin/shdo.py
+++ linux/home/bin/shdo.py
@@ -3,6 +3,8 @@ import glob
 import sys
 
 DATABASE = os.path.expanduser('~/.shdo/')
+if not os.path.exists(DATABASE):
+    os.makedirs(DATABASE)
 
 def _tag_list():
     return(os.listdir(DATABASE))
@@ -11,7 +13,10 @@ def _tag_targets(tag):
     return([line.rstrip('\n') for line in open(DATABASE + tag)])
 
 def add(args):
-    pass
+    tag = args[0]
+    targets = args[1:]
+    with open(DATABASE + '/' + tag, "a") as targets_file:
+        targets_file.write('\n'.join([os.path.abspath(os.path.expanduser(target)) for target in targets]) + '\n')
 
 def list(args):
     if len(args) == 0:
