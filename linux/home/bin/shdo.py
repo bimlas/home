941bccbb12bde58f9058fa84649593a032a5b746
bimlas <bimbalaszlo@gmail.com>
2017-06-02 15:05:44 +0200

    !bin_shdo: Improve arguments parsing

--- linux/home/bin/shdo.py
+++ linux/home/bin/shdo.py
@@ -3,36 +3,53 @@ import glob
 import sys
 
 DATABASE = os.path.expanduser('~/.shdo/')
+TAGS_PREFIX = '!'
+
 if not os.path.exists(DATABASE):
     os.makedirs(DATABASE)
 
-def _tag_list():
+def _parse_arguments(args):
+    args_copy = args[:]
+    tags = []
+    while len(args_copy) > 0 and args_copy[0][0] == TAGS_PREFIX:
+        # Remove '@' form tag name.
+        tags.append(args_copy.pop(0)[1:])
+    return({'tags': tags, 'other': args_copy})
+
+def _all_tags():
     return(os.listdir(DATABASE))
 
 def _tag_targets(tag):
+    if not os.path.exists(DATABASE + tag):
+        print('TAG NOT EXISTS: ' + tag)
+        return([])
     return([line.rstrip('\n') for line in open(DATABASE + tag)])
 
 def add(args):
     tag = args[0]
     targets = args[1:]
-    with open(DATABASE + '/' + tag, "a") as targets_file:
+    with open(DATABASE + tag, "a") as targets_file:
         targets_file.write('\n'.join([os.path.abspath(os.path.expanduser(target)) for target in targets]) + '\n')
 
 def list(args):
-    if len(args) == 0:
-        for tag in _tag_list():
-            print(tag)
+    args = _parse_arguments(args)
+    if len(args['other']) > 0:
+        print('Nothing to do with "' + ' '.join(args['other']) + '"')
+    if len(args['tags']) == 0:
+        print('==== ALL TAGS\n' + '\n'.join(_all_tags()))
     else:
-        for tag in args:
+        for tag in args['tags']:
             print('\n==== ' + tag + '\n' + '\n'.join(_tag_targets(tag)))
 
 def run(args):
-    for tag in _tag_list():
+    args = _parse_arguments(args)
+    for tag in args['tags']:
         print("\n==== " + tag)
         dirs = _tag_targets(tag)
         for dir in dirs:
             print("\n---- " + dir)
-            os.system('cd ' + dir + ' && git s')
+            os.system('cd ' + dir + ' && ' + ' '.join(args['other']))
+            # os.system('cd ' + dir + ' && git s')
 
 if len(sys.argv) == 1:
     sys.exit('HELP')
@@ -42,4 +59,4 @@ if sys.argv[1] == 'list':
 elif sys.argv[1] == 'add':
     add(sys.argv[2:])
 else:
-    run(sys.argv)
+    run(sys.argv[1:])
