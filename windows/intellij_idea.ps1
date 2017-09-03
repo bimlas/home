13c1796575242e8ba562a6e1cdb7edb3b97e353b
bimlas <bimbalaszlo@gmail.com>
2017-09-03 21:53:44 +0200

    !IDEA

---
+++ windows/intellij_idea.ps1
@@ -0,0 +1,10 @@
+$ideadir = "$ENV:HOMEDRIVE\$ENV:HOMEPATH\.IntelliJIdea*\config"
+
+Remove-Item "$ideadir\eval" -Recurse
+
+foreach ($optionsfile in Get-ChildItem "$ideadir\options\options.xml") {
+  Get-Content "$optionsfile" | Where-Object {$_ -notmatch 'evlsprt'} | Set-Content "$optionsfile.tmp"
+  Move-Item "$optionsfile.tmp" "$optionsfile" -Force
+}
+
+Get-ChildItem -Path "HKCU:\Software\JavaSoft\Prefs\jetbrains\idea" -Include * -ErrorAction SilentlyContinue | Remove-Item
