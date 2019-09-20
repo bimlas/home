f89ea26bc4037b09937613fc0264eda93b5d398a
bimlas <bimbalaszlo@gmail.com>
2019-09-20 07:35:33 +0200

    !conky: Fix media list to work on Manjaro

--- linux/home/bin/conky-media-list.sh
+++ linux/home/bin/conky-media-list.sh
@@ -2,7 +2,7 @@
 # conky-media-list: List mounted devices
 # https://forums.bunsenlabs.org/viewtopic.php?pid=74021#p74021
 
-df -h | grep '^/dev/' | grep -v '\s/snap/' | while read DEVICE SIZE USED FREE PERCENT MOUNT
+df -h | grep '^/dev/' | grep -v '/snap\(\.d\)\?/' | while read DEVICE SIZE USED FREE PERCENT MOUNT
 do
   echo "$MOUNT\${alignr} $SIZE - $USED = $FREE \${fs_bar 8,60 $MOUNT}"
 done
