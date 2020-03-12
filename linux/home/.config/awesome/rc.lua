26e187ab2247396b6e94da12a34dc47a911562a3
bimlas <bimbalaszlo@gmail.com>
2020-03-12 17:15:57 +0100

    Revert "!awesomewm: Mods needed to work at home"
    This reverts commit 455526ad95ca234c0738c82702e3b1d6f5bf0b71.
    
    Updated to AwesomeWM 4.3 at home, so I don't need old code.

--- linux/home/.config/awesome/rc.lua
+++ linux/home/.config/awesome/rc.lua
@@ -206,10 +206,18 @@ awful.screen.connect_for_each_screen(function(s)
                            awful.button({ }, 4, function () awful.layout.inc( 1) end),
                            awful.button({ }, 5, function () awful.layout.inc(-1) end)))
     -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
+    s.mytaglist = awful.widget.taglist {
+        screen  = s,
+        filter  = awful.widget.taglist.filter.all,
+        buttons = taglist_buttons
+    }
 
     -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
+    s.mytasklist = awful.widget.tasklist {
+        screen  = s,
+        filter  = awful.widget.tasklist.filter.currenttags,
+        buttons = tasklist_buttons
+    }
 
     -- Create the wibox
     s.mywibox = awful.wibar({ position = "top", height = 16, screen = s })
