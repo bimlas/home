9dfb8acbb712355e5d92cdb58e042949d3e7fc93
bimlas <bimbalaszlo@gmail.com>
2021-01-21 07:37:48 +0100

    !xfce_genmon: Replace '&' in tooltip messages
    It breaks the display.

--- linux/home/bin/xfce-genmon/vagrant-apache-log.sh
+++ linux/home/bin/xfce-genmon/vagrant-apache-log.sh
@@ -20,6 +20,6 @@ case "$2" in
     echo "<txt>Log: $1</txt>"
     echo "<img>/usr/share/icons/elementary-xfce/mimes/24/text.png</img>"
     echo "<click>xdg-open "$log_file"</click>"
-    echo "<tool><span font_family='monospace'>`cat "$log_file" | tail -5`</span></tool>"
+    echo "<tool><span font_family='monospace'>`cat "$log_file" | tail -5 | sed 's/&/&#38;/g'`</span></tool>"
     echo "<txtclick>"$0" "$1" update</txtclick>"
 esac
