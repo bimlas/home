cad557744e497c75f5ac93e2d65f45ae9e7406c6
bimlas <bimbalaszlo@gmail.com>
2021-02-02 14:14:08 +0100

    !bin!xfce_genmon: Merge presonal log script and XFCE GenMon widget

--- linux/home/bin/logadd.sh
+++ linux/home/bin/logadd.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Add timestamp to personal log file for time tracking
+#
+# Use as `/bin/sh -c "GENMON=1 logadd.sh"` if you want to use as XFCE GenMon
+# widget.
 
 log_file="$HOME/cuccok/download/mylog"
 
@@ -12,4 +15,11 @@ message=`
 
 if [ ! -z "$message" ]; then
   echo "`date +"%Y-%m-%d %T"` $message" >> $log_file
+fi
+
+if [ ! -z "$GENMON" ]; then
+  echo "<img>/usr/share/icons/elementary-xfce/mimes/24/text.png</img>"
+  echo "<txt>`cat $log_file | tail -1`</txt>"
+  echo "<tool>`cat $log_file | tail -20`</tool>"
+  echo "<txtclick>xterm -e /bin/bash --login -i -c $0</txtclick>"
 fi
