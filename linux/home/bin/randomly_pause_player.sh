41487d69f7468e4af3c859ea82a4893a47e042a7
bimlas <bimbalaszlo@gmail.com>
2021-02-03 18:45:34 +0100

    !bin: spotify_random_pause -> randomly_pause_player
    Let it work with any media player (Spotify, YouTube, VLC, etc).

---
+++ linux/home/bin/randomly_pause_player.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# randomly_pause_player: Randomly pause music for children's play
+#
+# Usage:
+#   watch randomly_pause_player
+#
+# Note:
+#   YouTube works only in Firefox
+
+if (( $RANDOM > 25000 )); then
+  playerctl --all-players play-pause
+  sleep 2
+  playerctl --all-players play-pause
+fi
