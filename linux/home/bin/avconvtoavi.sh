a706649c36e5de11ab3aae022ee7ab23daaefffd
bimlas <bimbalaszlo@gmail.com>
2021-05-18 22:24:45 +0200

    Drop old, unused files / configs

--- linux/home/bin/avconvtoavi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# avconvtoavi - convert video to avi
-
-avconv -i $1 -f avi -c:v msmpeg4 -qscale 4 -c:a libmp3lame -ac 2 -ab 192k -ar 44100 $1.avi
--- linux/home/bin/avconvtomp3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# avconvtoavi - convert video to avi
-
-avconv -i $1 -vn -f mp3 -ac 2 -ar 44100 -b:a 192k $1.mp3
