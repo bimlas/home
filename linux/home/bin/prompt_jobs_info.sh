1d27620ccb0cf8801b9918cd095b50d53e1da1da
bimlas <bimbalaszlo@gmail.com>
2021-05-26 23:29:37 +0200

    !bash!zsh: Refactor jobs prompt

--- linux/home/bin/prompt_jobs_info.sh
+++ linux/home/bin/prompt_jobs_info.sh
@@ -10,6 +10,15 @@ RED=$(echo -en "${JOBS_PROMPT_RED:-\e[1;31m}")
 GREEN=$(echo -en "${JOBS_PROMPT_GREEN:-\e[1;32m}")
 TEXT=$(echo -en "${JOBS_PROMPT_TEXT:-\e[0m}")
 
-formated_list=$(echo "$list_of_jobs" | sed -E 's/^(\[[0-9]+\])\s*([+-])?\s+(\S+)( \([^\)]+\))?\s+([^-\(]+).*/| \1\2 \3 \5/g' | tr -s '\n' ' ')
-colorized_list=$(echo "$formated_list" | sed -E -e "s/running /${GREEN}/gi" -e "s/suspended |continued |stopped /${RED}/gi" -e "s/\| /${TEXT}/g")
-echo -e "$colorized_list"
+function _standardize()
+{
+  sed -E 's/^(\[[0-9]+\])\s*([+-])?\s+(\S+)( \([^\)]+\))?\s+([^-\(]+).*/| \1\2 \3 \5/g' \
+  | tr -s '\n' ' '
+}
+
+echo "$list_of_jobs" \
+| _standardize \
+| sed -E \
+  -e "s/running /${GREEN}/gi" \
+  -e "s/suspended |continued |stopped /${RED}/gi" \
+  -e "s/\| /${TEXT}/g"
