bb2b045294d1814647b5f40e9da14a9d17633d30
bimlas <bimbalaszlo@gmail.com>
2021-05-26 23:13:18 +0200

    !bash!zsh: Use variables for jobs prompt colors

--- linux/home/bin/prompt_jobs_info.sh
+++ linux/home/bin/prompt_jobs_info.sh
@@ -6,6 +6,10 @@ if [[ -z "$list_of_jobs" ]]; then
   exit 1
 fi
 
-formated_list=$(echo "$list_of_jobs" | sed -E 's/^(\[[0-9]+\])\s*([+-])?\s+(\S+)( \([^\)]+\))?\s+([^-\(]+).*/\1\2 \3 \5 |/g' | tr -s '\n' ' ')
-colorized_list=$(echo "$formated_list" | sed -E -e "s/running /$2/gi" -e "s/suspended |continued |stopped /$3/gi" -e "s/ \|/$1/g")
-echo "$1$colorized_list"
+RED=$(echo -en "${JOBS_PROMPT_RED:-\e[1;31m}")
+GREEN=$(echo -en "${JOBS_PROMPT_GREEN:-\e[1;32m}")
+TEXT=$(echo -en "${JOBS_PROMPT_TEXT:-\e[0m}")
+
+formated_list=$(echo "$list_of_jobs" | sed -E 's/^(\[[0-9]+\])\s*([+-])?\s+(\S+)( \([^\)]+\))?\s+([^-\(]+).*/| \1\2 \3 \5/g' | tr -s '\n' ' ')
+colorized_list=$(echo "$formated_list" | sed -E -e "s/running /${GREEN}/gi" -e "s/suspended |continued |stopped /${RED}/gi" -e "s/\| /${TEXT}/g")
+echo -e "$colorized_list"
