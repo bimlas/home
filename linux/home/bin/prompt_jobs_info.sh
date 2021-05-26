8179a4fd6ebad69422b2aa9816be31e951c0736a
bimlas <bimbalaszlo@gmail.com>
2021-05-26 20:35:59 +0200

    !bash!zsh: Fix in Bash
    Before:
    
      $EDITOR function.sh
      Ctrl + Z
      cd ..
      Jobs: [1]+ $EDITOR function.sh (wd: ~/home)
    
    After:
    
      ...
      Jobs: [1]+ $EDITOR function.sh

--- linux/home/bin/prompt_jobs_info.sh
+++ linux/home/bin/prompt_jobs_info.sh
@@ -6,6 +6,6 @@ if [[ -z "$list_of_jobs" ]]; then
   exit 1
 fi
 
-formated_list=$(echo "$list_of_jobs" | sed -E 's/^(\[[0-9]+\])\s*([+-])?\s+(\S+)( \([^\)]+\))?\s+([^-]+).*/\1\2 \3 \5 |/g' | tr -s '\n' ' ')
+formated_list=$(echo "$list_of_jobs" | sed -E 's/^(\[[0-9]+\])\s*([+-])?\s+(\S+)( \([^\)]+\))?\s+([^-\(]+).*/\1\2 \3 \5 |/g' | tr -s '\n' ' ')
 colorized_list=$(echo "$formated_list" | sed -E -e "s/running /$2/gi" -e "s/suspended |continued |stopped /$3/gi" -e "s/ \|/$1/g")
 echo "$1$colorized_list"
