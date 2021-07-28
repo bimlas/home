87f7c30a2c2647e493590e61a46c7a46d9d9a827
bimlas <bimbalaszlo@gmail.com>
2021-07-28 15:38:04 +0200

    Add installers to install script

--- linux_install.sh
+++ linux_install.sh
@@ -23,6 +23,8 @@ git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+bash <(curl -sL https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install)
+
 sudo apt install -y vim
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash
