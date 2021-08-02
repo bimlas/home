49605722af88c9c8a55378a21db5d49a37b5f8d6
bimlas <bimbalaszlo@gmail.com>
2021-08-02 16:58:24 +0200

    Do not install what I don't use

--- linux_install.sh
+++ linux_install.sh
@@ -25,7 +25,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 bash <(curl -sL https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install)
 
-sudo apt install -y vim
+sudo apt install -y neovim
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash
 sudo apt install -y nodejs
@@ -36,13 +36,13 @@ sudo npm install -g diff-so-fancy
 
 sudo apt install -y gcc make
 
-sudo apt install -y openjdk-11-jdk
+#sudo apt install -y openjdk-11-jdk
 
-sudo apt install -y python python-pip python3 python3-pip
+sudo apt install -y python3 python3-pip
 
-sudo apt install -y ruby ruby-dev
+#sudo apt install -y ruby ruby-dev
 
-sudo snap install intellij-idea-ultimate --classic
+#sudo snap install intellij-idea-ultimate --classic
 
 # __ OTHER ______________________________
 
@@ -50,14 +50,14 @@ sudo npm install -g tiddlywiki
 
 # __ DESKTOP ____________________________
 
-sudo apt install -y vim-gtk
+#sudo apt install -y vim-gtk
 sudo apt install -y redshift
 # Only conky-all contains nVidia variables
 # sudo apt install -y conky-all
 # AutoHotKey for Linux, xautomation is needed for `xte` only
 sudo apt install -y xbindkeys # xautomation
 # Fuzzy finder GUI
-sudo apt install -y rofi
+#sudo apt install -y rofi
 
 # Spotify
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
@@ -68,7 +68,7 @@ sudo apt install -y spotify-client
 # __ NET ________________________________
 
 sudo apt install -y qbittorrent
-sudo apt install -y telegram-desktop
+#sudo apt install -y telegram-desktop
 
 # Vivaldi
 curl --location -o ./vivaldi.deb https://downloads.vivaldi.com/stable/vivaldi-stable_amd64.deb
@@ -77,16 +77,16 @@ sudo apt install -y --fix-broken
 rm ./vivaldi.deb
 
 # Skype
-curl --location -o ./skype.deb https://go.skype.com/skypeforlinux-64.deb
-sudo dpkg -i ./skype.deb
-sudo apt install -y --fix-broken
-rm ./skype.deb
+#curl --location -o ./skype.deb https://go.skype.com/skypeforlinux-64.deb
+#sudo dpkg -i ./skype.deb
+#sudo apt install -y --fix-broken
+#rm ./skype.deb
 
 # TeamViewer
-curl --location -o ./teamviewer.deb https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-sudo dpkg -i ./teamviewer.deb
-sudo apt install -y --fix-broken
-rm ./teamviewer.deb
+#curl --location -o ./teamviewer.deb https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+#sudo dpkg -i ./teamviewer.deb
+#sudo apt install -y --fix-broken
+#rm ./teamviewer.deb
 
 # To autostart window manager:
 #   $ cat 'exec awesome' >> ~/.xinitrc OR /etc/X11/xinit/.xinitrc
